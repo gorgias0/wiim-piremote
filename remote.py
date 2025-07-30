3e321e89ad326a9d39e29d450111f8f54c47df72
Daniel Lindkvist <gorgias0@gmail.com>
2025-07-30 18:44:19 +0200

    Initial commit

---
+++ remote.py
@@ -0,0 +1,50 @@
+import lirc # type: ignore
+import requests # type: ignore
+
+# ip of the wiim device on local network
+BASE_URL="https://192.168.0.241/httpapi.asp?command="
+
+def send(command):
+    requests.get(BASE_URL + command, verify=False) # ignore SSL warnings
+
+def ProcessIRRemote():
+
+    #get IR command
+    #keypress format = (hexcode, repeat_num, command_key, remote_id)
+    try:
+        keypress = conn.readline()
+    except:
+        keypress=""
+
+    if (keypress != "" and keypress != None):
+
+        data = keypress.split()
+        sequence = data[1]
+        command = data[2]
+
+        #ignore command repeats
+        if (sequence != "00"):
+           return
+
+        # uncomment to see what commands your remote sends
+        # print(command) 
+
+        if (command == "KEY_DOWN"):
+            send("setPlayerCmd:vol--")
+        if (command == "KEY_UP"):
+            send("setPlayerCmd:vol%2b%2b")
+        if (command == "KEY_LEFT"):
+            send("setPlayerCmd:prev")
+        if (command == "KEY_RIGHT"):
+            send("setPlayerCmd:next")
+        if (command == "KEY_ENTER"):
+            send("setPlayerCmd:onepause")
+
+#define Global
+client = lirc.Client()
+conn = lirc.LircdConnection()
+conn.connect()
+print("Listening...")
+
+while True:
+      ProcessIRRemote()
